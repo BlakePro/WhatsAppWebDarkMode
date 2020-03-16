8e44d1cda7388267156345c6cb887fe0dcf69381
Cristian Yosafat Hernández Ruiz <blakegt@gmail.com>
2020-03-15 19:21:30 -0600

    Update background.js

--- WhatsAppDarkModeChromeExtension/background.js
+++ WhatsAppDarkModeChromeExtension/background.js
@@ -1,8 +1,4 @@
-// this is the background code...
-
-// listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.ib, {
 		file: 'whatsapp_black.js'
 	});
